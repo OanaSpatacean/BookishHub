bab3613c4ebf9aa13a242020b07e42596e45ec91
OanaSpatacean <126173446+OanaSpatacean@users.noreply.github.com>
2024-12-19 23:54:40 +0200

    Added info to the user about membership

--- bookishhub/app/configurations/page.tsx
+++ bookishhub/app/configurations/page.tsx
@@ -1,6 +1,8 @@
+import CreateMembership from "@/components/CreateMembership";
 import { getAuthSession } from "@/lib/authentication";
 import { redirect } from "next/navigation";
 import verifyMembership from "@/lib/membership";
+import { InfoIcon } from "lucide-react";
 
 type Props = {};
 
@@ -33,14 +35,43 @@ const Configurations = async (props: Props) => {
             </h1>
 
             {isPowerAccount ? (
-                <p className="text-secondary-foreground/70 text-3xl">
+                <p className="text-secondary-foreground/50 text-3xl text-center mt-4">
                     You have a Power BookishHub account!
                 </p>
             ) : (
-                <p className="text-secondary-foreground/70 text-3xl">
+                <p className="text-secondary-foreground/50 text-3xl text-center mt-4">
                     You don't have a Power BookishHub account!
                 </p>
             )}
+
+<div className="bg-secondary 
+                border-none 
+                p-4 
+                flex
+                mt-7
+                mb-8">
+                <div className="flex-shrink-0">
+                    <InfoIcon 
+                        className="text-green-500 
+                                   h-10 
+                                   w-10 
+                                   bg-green-100 
+                                   rounded-full 
+                                   p-2 
+                                   shadow-sm"/>
+                </div>
+
+                <div className="ml-5">
+                Welcome to BookishHub! We're excited to help you unlock the full potential of your membership experience.
+
+If you don’t already have a membership, you can easily join by pressing the button below. Becoming a member grants you access to a wide range of exclusive features, including custom tools, advanced resources, and priority support to help you achieve your goals with ease and efficiency.
+
+Already have a membership? Fantastic! As a valued member, you’ll become part of the Power BookishHub community, an exclusive group of individuals who enjoy enhanced benefits and premium access to everything BookishHub has to offer. You can also take your membership further by unlocking additional design options, giving you even more flexibility and creative possibilities.
+
+Whether you’re starting a new membership or upgrading an existing one, everything you need is just a click away. Press the button below to join the Power BookishHub community or access even more design options to elevate your experience.                </div>
+            </div>
+            
+            <CreateMembership isPowerAccount={isPowerAccount} />
         </div>
     )
 }
---
+++ bookishhub/components/CreateMembership.tsx
@@ -0,0 +1,55 @@
+"use client";
+import axios from "axios"
+import React from "react";
+import { Button } from "./ui/button";
+
+type Props = 
+{
+    isPowerAccount: boolean
+}
+
+const CreateMembership = ({isPowerAccount}:Props) => {
+    const [loading, setLoading] = React.useState(false);
+
+    const handleSubscribe = async () => 
+    {
+        setLoading(true);
+
+        try 
+        {
+            const response = await axios.get("/api/payment");
+            window.location.href = response.data.url;
+        } 
+        catch (error) 
+        {
+            console.log("There was a payment error");
+        } 
+        finally 
+        {
+            setLoading(false);
+        }
+    }
+    
+    return (
+        <Button onClick={handleSubscribe} disabled={loading} className="text-white 
+                                                                        transition 
+                                                                        bg-gradient-to-r 
+                                                                        from-blue-500 
+                                                                        to-blue-900 
+                                                                        hover:from-blue-600 
+                                                                        hover:to-blue-800 
+                                                                        w-full 
+                                                                        mb-6 
+                                                                        text-md 
+                                                                        font-semibold">
+            {
+                isPowerAccount ? 
+                "Get your monthly membership" 
+                : 
+                "Get more designs"
+            }
+        </Button>
+  )
+}
+
+export default CreateMembership;
