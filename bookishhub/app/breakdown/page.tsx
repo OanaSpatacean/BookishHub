969dc959082b12a0d55ad46c95b864c32e5ce2e5
OanaSpatacean <126173446+OanaSpatacean@users.noreply.github.com>
2025-01-16 15:56:20 +0200

    Worked on PDFbreakdown page

--- bookishhub/app/breakdown/page.tsx
+++ bookishhub/app/breakdown/page.tsx
@@ -1,7 +1,7 @@
 import PDFDrop from '@/components/PDFDrop';
 import { getAuthSession } from '@/lib/authentication';
 import verifyMembership from '@/lib/membership';
-import { ArrowRight } from 'lucide-react';
+import { ArrowRight, InfoIcon } from 'lucide-react';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
 import React, { useState } from 'react'
@@ -48,13 +48,33 @@ const PDFBreakdown = async (props: Props) => {
                                                     items-center 
                                                     text-md
                                                     font-semibold">
-                        Click here to break down your PDF
-                        <ArrowRight strokeWidth={5} className="ml-[850px] 
-                                                            h-6 
-                                                            w-6"/>
+                        Click here to converse with our AI about your PDFs
+                        <ArrowRight strokeWidth={5} className="ml-[750px] 
+                                                               h-6 
+                                                               w-6"/>
                     </Link>
             </div>
             <PDFDrop/>
+            <div className="bg-secondary 
+                      border-none 
+                      p-4 
+                      flex
+                      mt-5">
+                <div className="flex-shrink-0">
+                    <InfoIcon 
+                        className="text-green-500 
+                                    h-10 
+                                    w-10 
+                                    bg-green-100 
+                                    rounded-full 
+                                    p-2 
+                                    shadow-sm" />
+                </div>
+
+                <div className="ml-5">
+                    Hi! Here, you can easily upload your PDF file and feed the information into our AI. Once uploaded, you can converse with the AI and ask any questions about the contents of your document. This will save you time and effort, as you no longer have to read through lengthy pages—our AI is here to help you break down your PDFs and assist with any query related to the text. To start your conversation with our AI, simply click the button above. You'll be able to see all the PDFs you've uploaded, and if this is your first time, just upload your file, and you're good to go! Enjoy the convenience and power of AI-assisted reading.
+                </div>
+            </div>
         </div>              
     );
 };
