65940129b1bf1fc83382530e04809ea532133a89
OanaSpatacean <126173446+OanaSpatacean@users.noreply.github.com>
2025-01-20 16:47:45 +0200

    Create route.ts

---
+++ bookishhub/app/api/PDFRequestResponse/route.ts
@@ -0,0 +1,13 @@
+import { databaseClient } from '@/lib/database';
+import { NextResponse } from 'next/server';
+
+export const POST = async (request: Request) => {
+  try 
+  {
+
+  } 
+  catch (error) 
+  {
+    
+  }
+}
