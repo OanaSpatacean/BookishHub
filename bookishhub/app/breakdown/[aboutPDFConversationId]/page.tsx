95103f9a427613fad79b2d0480989e295586fc60
OanaSpatacean <126173446+OanaSpatacean@users.noreply.github.com>
2025-01-20 12:41:40 +0200

    Listed all conversations

--- bookishhub/app/breakdown/[aboutPDFConversationId]/page.tsx
+++ bookishhub/app/breakdown/[aboutPDFConversationId]/page.tsx
@@ -1,31 +1,63 @@
+import AllConversationsListed from "@/components/AllConversationsListed";
 import { getAuthSession } from "@/lib/authentication";
+import { databaseClient } from "@/lib/database";
 import verifyMembership from "@/lib/membership";
 import { redirect } from "next/navigation";
 import React from "react";
 
-type Props = {
-    params: {
+type Props = 
+{
+    params:
+    {
         aboutPDFConversationId: string;
-}}
+    }
+}
 
 const AboutPDFConversationPage = async ({ params: { aboutPDFConversationId } }: Props) => {
     const session = await getAuthSession();
 
-    if(!session?.user){ 
+    if(!session?.user)
+    { 
         return redirect('/');
     }
 
     const havePowerAccount = verifyMembership();
+
+    const userPDFConversations = await databaseClient.aboutPDFConversations.findMany({
+        where: 
+        { 
+            userId: session.user.id 
+        }
+    })
+
+    if (!userPDFConversations || userPDFConversations.length === 0) 
+    {
+        return redirect("/breakdown");
+    }
+    
+    const currentConversation = userPDFConversations.find(
+        (aboutPDFConversation) => aboutPDFConversation.id === parseInt(aboutPDFConversationId)
+    )
+
+    if (!currentConversation) 
+    {
+    return redirect("/breakdown");
+    }
     
     return (
-        <div className="bg-purple-50 
-                        p-1 
-                        rounded-2xl 
-                        px-4 
-                        py-4 
-                        h-[300px]">
-            AboutPDFConversationPage
-        </div>         
+        <div className="h-screen 
+                        flex">
+            <div className="h-screen 
+                            w-full 
+                            flex">
+                <div className="max-w-xs 
+                                h-screen 
+                                flex-[1]">
+                    <AllConversationsListed userPDFConversations={userPDFConversations} aboutPDFConversationId={currentConversation.id} havePowerAccount={havePowerAccount} />
+                </div>
+
+            </div>
+        </div>  
     )
 }
 
---
+++ bookishhub/components/AllConversationsListed.tsx
@@ -0,0 +1,76 @@
+"use client"
+import { cn } from "@/lib/utils";
+import { AboutPDFConversations } from "@prisma/client";
+import { Plus } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { TbMessageChatbotFilled } from "react-icons/tb";
+
+type Props = 
+{
+    userPDFConversations: AboutPDFConversations[];
+    aboutPDFConversationId: number;
+    havePowerAccount: any;
+}
+
+const AllConversationsListed = ({userPDFConversations, aboutPDFConversationId, havePowerAccount}: Props) => {
+    const [loading, setLoading] = React.useState(false);
+
+    return (
+        <div className="dark:text-gray-100 
+                        mt-5 
+                        w-full 
+                        p-4 
+                        text-gray-900 
+                        h-screen 
+                        bg-gray-200 
+                        dark:bg-gray-800 
+                        overflow-scroll">
+            <Link href="/breakdown">
+                <Button className="w-full">
+                    <Plus className="w-full 
+                                     mr-2 
+                                     h-4" />
+                    Create new PDF conversation
+                </Button>
+            </Link>
+  
+        <div className="mt-5 
+                        pb-20 
+                        max-h-screen 
+                        flex 
+                        gap-2 
+                        flex-col">
+          {userPDFConversations.map((userPDFConversation) => (
+            <Link key={userPDFConversation.id} href={`/breakdown/${userPDFConversation.id}`}>
+              <div className={cn("rounded-lg p-3 flex items-center",
+                                {"bg-purple-600 text-white hover:bg-purple-500 hover:dark:bg-purple-900 dark:text-white":
+                                    userPDFConversation.id === aboutPDFConversationId,
+                                "bg-gray-200 text-gray-800 dark:bg-gray-800 dark:text-gray-200 hover:dark:bg-gray-600 hover:dark:text-white":
+                                    userPDFConversation.id !== aboutPDFConversationId
+                                },
+                                userPDFConversation.id === aboutPDFConversationId ? "hover:bg-purple-500"
+                                : "hover:bg-gray-300 hover:text-gray-900"
+                            )}>
+
+                <TbMessageChatbotFilled  className="mr-2"/>
+
+                <p className="whitespace-nowrap 
+                              text-sm 
+                              truncate 
+                              w-full 
+                              text-ellipsis 
+                              overflow-hidden">
+                  {userPDFConversation.pdfName}
+                </p>
+
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div> 
+    )
+}
+
+export default AllConversationsListed;
