1b6af72daa7635ac32add5ad1e650e017fcbf958
OanaSpatacean <126173446+OanaSpatacean@users.noreply.github.com>
2025-01-25 19:27:59 +0200

    Update PDFRequest.ts

--- bookishhub/app/form-validators/PDFRequest.ts
+++ bookishhub/app/form-validators/PDFRequest.ts
@@ -1,11 +1,9 @@
 import { z } from 'zod'
 
 export const PDFRequestSchema = z.object({
-    aboutPDFConversationId: z.number(),
-    PDFRequests: z.array(
-      z.object({
-        content: z.string().min(1).max(5000),
-        role: z.enum(["SYSTEM", "USER"]),
-      })
-    )
+  id: z.number().int(),
+  aboutPDFConversationId: z.number().int(),
+  content: z.string(),
+  createdAt: z.string().datetime(),
+  role: z.enum(['SYSTEM', 'USER'])
 })
