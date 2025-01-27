c554f9913e91d4d1bcaf1ad7b3f4e43be9600f15
OanaSpatacean <126173446+OanaSpatacean@users.noreply.github.com>
2025-01-27 20:27:49 +0200

    Modified s3.ts to be able to upload any pdf

--- bookishhub/lib/s3.ts
+++ bookishhub/lib/s3.ts
@@ -8,22 +8,24 @@ export function getS3Url(keyOfFile: string)
 
 export async function uploadToS3(file: File): Promise<{ keyOfFile: string; nameOfFile: string }> 
 {
-  return new Promise((resolve, reject) => 
+  return new Promise(async (resolve, reject) => 
   {
     try 
     {
-        const s3 = new S3({region: "eu-west-3",credentials: {accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID!, secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY!}})
+      const s3 = new S3({region: "eu-west-3",credentials: {accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID!, secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY!}})
 
-        const keyOfFile = file.name.replace(" ", "-");
+      const keyOfFile = file.name.replace(/ /g, "-");
+      const fileContent = await file.arrayBuffer(); 
 
-        const params = {Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,Key: keyOfFile,Body: file,ACL: 'public-read'}
+      const params = {Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,Key: keyOfFile,Body: fileContent,ContentType: file.type}
 
-        s3.putObject(params,(err: any, data: PutObjectCommandOutput | undefined) => {   return resolve({
-                                                                                        keyOfFile,
-                                                                                        nameOfFile: file.name
-                                                                                     })})
+      const data: PutObjectCommandOutput = await s3.putObject(params);
+      console.log("Upload successful:", data);
+
+      return resolve({ keyOfFile, nameOfFile: file.name });
     } 
     catch (error) 
     {
-      reject(error)
+      console.error("Error in uploadToS3:", error);
+      reject(error);
     }})}
