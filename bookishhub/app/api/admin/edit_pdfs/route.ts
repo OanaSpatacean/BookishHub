30b61071f6af584a0eafb7d8408a598d136e77a8
OanaSpatacean <126173446+OanaSpatacean@users.noreply.github.com>
2025-02-03 12:21:01 +0200

    Added delete button for pdf conversation

---
+++ bookishhub/app/api/admin/edit_pdfs/route.ts
@@ -0,0 +1,67 @@
+import { databaseClient } from "@/lib/database";
+import { ZodError, z } from "zod";
+import { NextResponse } from "next/server";
+
+const parseBody = z.object({ PDFConversationId: z.number() });
+
+export async function DELETE(request: Request) {
+    try 
+    {
+        const body = await request.json();
+        const { PDFConversationId } = parseBody.parse(body);
+
+        const pdfConversation = await databaseClient.aboutPDFConversations.findUnique({
+            where: 
+            {
+                id: PDFConversationId,
+            },
+            include: 
+            {
+                PDFRequests: true
+            }
+        })
+
+        if (!pdfConversation) 
+        {
+            return NextResponse.json(
+                { success: false, error: "PDF conversation does not exist!" },
+                { status: 404 }
+            )
+        }
+
+        if (pdfConversation.PDFRequests.length > 0) {
+            await databaseClient.pDFRequest.deleteMany({
+                where: 
+                {
+                    id: 
+                    {
+                        in: pdfConversation.PDFRequests.map((request) => request.id)
+                    }
+                }
+            })
+        }
+
+        await databaseClient.aboutPDFConversations.delete({
+            where: 
+            {
+                id: PDFConversationId,
+            }
+        })
+
+        return NextResponse.json({
+            success: true,
+            message: "PDF conversation and related requests deleted successfully"
+        })
+    } 
+    catch (error) 
+    {
+        console.error("Error processing delete request:", error);
+
+        if (error instanceof ZodError) 
+        {
+            return new NextResponse("Incorrect body format", { status: 400 });
+        }
+
+        return new NextResponse("Internal server error", { status: 500 });
+    }
+}
--- bookishhub/components/AllConversationsListedAdmin.tsx
+++ bookishhub/components/AllConversationsListedAdmin.tsx
@@ -1,10 +1,12 @@
 "use client";
-
 import { cn } from "@/lib/utils";
 import { AboutPDFConversations } from "@prisma/client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { TbMessageChatbotFilled } from "react-icons/tb";
+import { useToast } from "./ui/use-toast";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
 
 type Props = 
 {
@@ -13,6 +15,33 @@ type Props =
 }
 
 const AllConversationsListedAdmin = ({ userPDFConversations, aboutPDFConversationId }: Props) => {
+    const { toast } = useToast();
+    const [loadingId, setLoadingId] = useState<number | null>(null); 
+
+    const { mutate: deletePDFConversation } = useMutation({
+        mutationFn: async (input: { PDFConversationId: number }) => {
+            setLoadingId(input.PDFConversationId); 
+            const response = await axios.delete("/api/admin/edit_pdfs", { data: input });
+            return response.data;
+        },
+        onSuccess: () => {
+            toast({
+                title: "Success",
+                description: "PDF conversation deleted successfully",
+            });
+            window.location.reload();
+        },
+        onError: (error) => {
+            console.error("Error deleting PDF conversation:", error);
+            toast({
+                title: "Warning",
+                description: "An error occurred while deleting the PDF conversation",
+                variant: "destructive",
+            });
+            setLoadingId(null); 
+        }
+    })
+
     return (
         <div className="w-full 
                         flex 
@@ -20,16 +49,17 @@ const AllConversationsListedAdmin = ({ userPDFConversations, aboutPDFConversatio
                         gap-5">
             {userPDFConversations.map((conversation) => (
                 <div key={conversation.id} className={cn("rounded-lg border p-4 flex items-center bg-gray-50 dark:bg-gray-900 w-full",
-                                                        {
-                                                            "bg-green-600 text-white hover:bg-green-500 dark:hover:bg-green-700":
-                                                                conversation.id === aboutPDFConversationId,
-                                                            "hover:bg-gray-300 dark:hover:bg-gray-800":
-                                                                conversation.id !== aboutPDFConversationId,
-                                                        }
-                                                      )}>
+                                                            {
+                                                                "bg-green-600 text-white hover:bg-green-500 dark:hover:bg-green-700":
+                                                                    conversation.id === aboutPDFConversationId,
+                                                                "hover:bg-gray-300 dark:hover:bg-gray-800":
+                                                                    conversation.id !== aboutPDFConversationId,
+                                                            }
+                                                        )}>
                     <TbMessageChatbotFilled className="mr-3 
                                                        text-xl 
-                                                       text-primary" />
+                                                       text-primary"/>
+
                     <Link href={`/breakdown/${conversation.id}`} className="flex-grow">
                         <h3 className="text-primary 
                                        truncate 
@@ -38,6 +68,20 @@ const AllConversationsListedAdmin = ({ userPDFConversations, aboutPDFConversatio
                             {conversation.pdfName}
                         </h3>
                     </Link>
+
+                    <div className="justify-end 
+                                    flex">
+                        <div className="text-secondary-foreground/70">
+                            <button onClick={() => deletePDFConversation({ PDFConversationId: conversation.id })} className="underline 
+                                                                                                                             text-red-500 
+                                                                                                                             block 
+                                                                                                                             w-fit 
+                                                                                                                             disabled:opacity-50 
+                                                                                                                             mt-2" disabled={loadingId === conversation.id}>
+                                {loadingId === conversation.id ? "Deleting..." : "Delete PDF conversation"}
+                            </button>
+                        </div>
+                    </div>
                 </div>
             ))}
         </div>
