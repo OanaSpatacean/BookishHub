901256348d38554244aa468fe4515f20a70f9179
OanaSpatacean <126173446+OanaSpatacean@users.noreply.github.com>
2025-02-03 17:32:24 +0200

    Added update operation for pdf requests

--- bookishhub/app/api/admin/edit_PDF_request/route.ts
+++ bookishhub/app/api/admin/edit_PDF_request/route.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 import { NextResponse } from "next/server";
 
 const parseBody = z.object({ PDFRequestId: z.number() });
+const parseUpdateBody = z.object({PDFRequestId: z.number(), content: z.string().min(1, "Content cannot be empty")});
 
 export async function DELETE(request: Request) 
 {
@@ -53,3 +54,48 @@ export async function DELETE(request: Request)
     }
 }
 
+export async function PATCH(request: Request) {
+    try 
+    {
+        const body = await request.json();
+        const parsedBody = parseUpdateBody.safeParse(body);
+
+        if (!parsedBody.success) 
+        {
+            console.error("Validation error:", parsedBody.error);
+            return NextResponse.json({ success: false, error: "Invalid request format" }, { status: 400 });
+        }
+
+        const { PDFRequestId, content } = parsedBody.data;
+
+        const PDFRequest = await databaseClient.pDFRequest.findUnique({
+            where: 
+            {
+                id: PDFRequestId 
+            }
+        })
+
+        if (!PDFRequest) 
+        {
+            return NextResponse.json(
+                { success: false, error: "PDF request does not exist!" },
+                { status: 404 }
+            )
+        }
+
+        await databaseClient.pDFRequest.update({
+            where: { id: PDFRequestId },
+            data: { content }
+        })
+
+        return NextResponse.json({
+            success: true,
+            message: "PDF request content updated successfully"
+        })
+    } 
+    catch (error) 
+    {
+        console.error("Error processing update PDF request:", error);
+        return new NextResponse("Internal server error", { status: 500 });
+    }
+}
--- bookishhub/app/form-validators/PDFRequest.ts
+++ bookishhub/app/form-validators/PDFRequest.ts
@@ -2,6 +2,7 @@ import { z } from 'zod'
 
 export const PDFRequestSchema = z.object({
   PDFRequestId: z.number(),
+  content: z.string().min(1, "Content cannot be empty"),
   aboutPDFConversationId: z.number(),
   messages: z.array(
     z.object({
