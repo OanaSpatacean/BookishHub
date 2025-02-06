b52e16b781f8d71a31e4c93abbc43d48375ad4db
OanaSpatacean <126173446+OanaSpatacean@users.noreply.github.com>
2025-02-06 16:35:46 +0200

    Created repo page

---
+++ bookishhub/app/repo/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Loader2 } from "lucide-react";
+
+type Props = {};
+
+const RepoPage = (props: Props) => {
+  return (
+    <div className="">
+        Repository
+    </div>
+  )
+}
+
+export default RepoPage;
