4c10d198a0b68fd509480086544e42e770d1271c
OanaSpatacean <126173446+OanaSpatacean@users.noreply.github.com>
2025-02-13 16:11:35 +0200

    Update page.tsx

--- bookishhub/app/draft/page.tsx
+++ bookishhub/app/draft/page.tsx
@@ -7,24 +7,24 @@ const DraftAssist = (props: Props) => {
 
   return (
     <div className="flex 
-                        flex-col 
-                        items-start 
-                        mx-auto 
-                        px-15  
-                        max-w-7xl
-                        mt-7">
-            <h1 className="sm:text-5xl 
-                           text-left 
-                           font-bold 
-                           text-3xl 
-                           underline 
-                           decoration-4 
-                           decoration-purple-500
-                           mb-5">
-                Draft assist 
-            </h1>
+                    flex-col 
+                    items-start 
+                    mx-auto 
+                    px-15  
+                    max-w-7xl
+                    mt-7">
+        <h1 className="sm:text-5xl 
+                        text-left 
+                        font-bold 
+                        text-3xl 
+                        underline 
+                        decoration-4 
+                        decoration-purple-500
+                        mb-5">
+            Draft assist 
+        </h1>
 
-            <NewMemoryEnhancementCard/>
+        <NewMemoryEnhancementCard/>
     </div>
   )
 }
