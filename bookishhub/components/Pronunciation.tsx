87185472dcdcaab1d8405822b3b8feb887fcb136
OanaSpatacean <126173446+OanaSpatacean@users.noreply.github.com>
2025-03-04 20:13:39 +0200

    Update Pronunciation.tsx

--- bookishhub/components/Pronunciation.tsx
+++ bookishhub/components/Pronunciation.tsx
@@ -108,7 +108,8 @@ const Pronunciation = ({ language, languageSession, pronunciationWords }: Props)
                                                                                                 text-white 
                                                                                                 px-4 
                                                                                                 py-2 
-                                                                                                rounded">
+                                                                                                rounded
+                                                                                                hover:bg-purple-800">
                     <FiMic className="w-5 
                                       h-5"/> 
                     <span>
