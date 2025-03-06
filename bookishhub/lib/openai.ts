dde3fbdb795f2f0519941bf6c8b663d860cb56d4
OanaSpatacean <126173446+OanaSpatacean@users.noreply.github.com>
2025-03-06 09:54:34 +0200

    Updated openai version

--- bookishhub/lib/openai.ts
+++ bookishhub/lib/openai.ts
@@ -1,9 +1,10 @@
-import { Configuration, OpenAIApi } from "openai";
+import OpenAI from "openai";
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY, 
 });
-export const openai = new OpenAIApi(configuration);
+
+export default openai;
 
 interface OutputFormat {
   [key: string]: string | string[] | OutputFormat;
