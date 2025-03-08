d58b1335f783125fccc68b9f8f89884ae5418e6d
OanaSpatacean <126173446+OanaSpatacean@users.noreply.github.com>
2025-03-08 19:57:54 +0200

    Create route.ts

---
+++ bookishhub/app/api/language/listening/create_listening_exercises/route.ts
@@ -0,0 +1,101 @@
+import { ZodError } from "zod";
+import { NextResponse } from "next/server";
+import { databaseClient } from "@/lib/database";
+import { getAuthSession } from "@/lib/authentication";
+import { createRephrasingSchema } from "@/app/form-validators/language";
+import { strict_output } from "@/lib/openai";
+import { generateAudio } from "@/lib/replicate"; 
+
+export async function POST(request: Request, response: Response) {
+    try {
+        const session = await getAuthSession();
+
+        if (!session?.user) 
+        {
+            return NextResponse.redirect("/");
+        }
+
+        const body = await request.json();
+        const parsedData = createRephrasingSchema.parse(body);
+
+        const languageId = parseInt(parsedData.languageId);
+        const languageSessionId = parseInt(parsedData.languageSessionId);
+        const level = parsedData.level;
+
+        const language = await databaseClient.language.findUnique({
+            where: 
+            { 
+                id: languageId 
+            },
+            select: 
+            { 
+                name: true 
+            }
+        })
+
+        if (!language) 
+        {
+            return new NextResponse("Invalid language ID", { status: 400 });
+        }
+
+        const languageSession = await databaseClient.languageSession.findFirst({
+            where: 
+            { 
+                id: languageSessionId 
+            }
+        })
+
+        if (!languageSession) 
+        {
+            return new NextResponse("No existing session found", { status: 404 });
+        }
+
+        const existingExercises = await databaseClient.listeningExercise.findMany({
+            where: 
+            { 
+                sessionId: languageSession.id 
+            }
+        })
+
+        if (existingExercises.length > 0) 
+        {
+            return NextResponse.json({ sessionId: languageSession.id, exercises: existingExercises });
+        }
+
+        const listeningExercises = await strict_output(
+            `Generate 5 simple words or short phrases for the ${language.name} language at the ${level} level. 
+            These words will be used for listening exercises.`,
+            new Array(5).fill(`Provide a single word or short phrase.`),
+            { phrase: "A simple word or phrase in the target language." }
+        )
+
+        const exercisesWithAudio = await Promise.all(
+            listeningExercises.map(async (exercise: { phrase: any; }) => {
+                const audioUrl = await generateAudio(language.name, exercise.phrase);
+                return {
+                    audioUrl,
+                    correctText: exercise.phrase,
+                    userAnswer: "",
+                    sessionId: languageSession.id
+                }
+            })
+        )
+
+        await databaseClient.listeningExercise.createMany({
+            data: exercisesWithAudio
+        })
+
+        return NextResponse.json({ sessionId: languageSession.id, exercises: exercisesWithAudio });
+    } 
+    catch (error) 
+    {
+        console.error("Error processing request:", error);
+
+        if (error instanceof ZodError) 
+        {
+            return new NextResponse("Incorrect body format", { status: 400 });
+        }
+
+        return new NextResponse("Internal server error", { status: 500 });
+    }
+}
