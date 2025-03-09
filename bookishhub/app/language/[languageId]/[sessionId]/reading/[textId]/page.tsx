7bb476602f8658e4ac45941866f1334197e269c1
OanaSpatacean <126173446+OanaSpatacean@users.noreply.github.com>
2025-03-09 15:06:35 +0200

    Added page for reading

---
+++ bookishhub/app/language/[languageId]/[sessionId]/reading/[textId]/page.tsx
@@ -0,0 +1,93 @@
+import Reading from "@/components/Reading";
+import { getAuthSession } from "@/lib/authentication";
+import { databaseClient } from "@/lib/database";
+import { redirect } from "next/navigation";
+
+type Props = 
+{
+    params:
+    {
+        languageId: string;
+        sessionId: string;
+        textId: string;
+    }
+}
+
+const SessionPageReading = async ({ params }: Props) => {
+    const session = await getAuthSession();
+
+    if (!session?.user) 
+    {
+        return redirect("/");
+    }
+
+    const { languageId, sessionId, textId } = params;
+
+    const language = await databaseClient.language.findUnique({
+        where: 
+        {
+            id: parseInt(languageId),
+        }
+    })
+
+    if (!language) 
+    {
+        return <div>Language not found</div>;
+    }
+
+    const languageSession = await databaseClient.languageSession.findUnique({
+        where: 
+        {
+            id: parseInt(sessionId),
+        }
+    })
+
+    if (!languageSession) 
+    {
+        return <div>Session not found</div>;
+    }
+
+    const readings = await databaseClient.reading.findMany({
+        where: 
+        {
+            sessionId: parseInt(sessionId),
+        }
+    })
+
+    const text = await databaseClient.textWriting.findUnique({
+        where: 
+        {
+          id: textId
+        }
+    })
+
+    if (!text) 
+    {
+        return <div>Text not found</div>;
+    }
+
+    return (
+        <div className="flex 
+                        flex-col 
+                        items-start 
+                        mx-auto 
+                        px-15 
+                        max-w-7xl 
+                        mt-7">
+            <h1 className="sm:text-5xl 
+                           text-left 
+                           font-bold 
+                           text-3xl 
+                           underline 
+                           decoration-4 
+                           decoration-purple-500 
+                           mb-5">
+                Session {languageSession.id} - {languageSession.level} level - {language.name}
+            </h1>
+
+            <Reading language={language} languageSession={languageSession} readings={readings} text={text}/>
+        </div>
+    );
+};
+
+export default SessionPageReading;
