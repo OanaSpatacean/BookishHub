9cd7373bfbce3b9164eb89c103d95377697d90a9
OanaSpatacean <126173446+OanaSpatacean@users.noreply.github.com>
2025-03-25 15:59:56 +0200

    Changed styling to verification page

--- bookishhub/app/verify-email/page.tsx
+++ bookishhub/app/verify-email/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { useToast } from '@/components/ui/use-toast';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
@@ -6,6 +7,7 @@ export default function VerifyEmail() {
     const searchParams = useSearchParams();
     const [message, setMessage] = useState("Verifying...");
     const router = useRouter();
+    const { toast } = useToast();
 
     useEffect(() => {
         async function verify() {
@@ -22,19 +24,37 @@ export default function VerifyEmail() {
 
             if (res.ok) 
             {
-                setMessage("Email successfully verified! You can now log in.");
+                toast({ title: "Success", description: "Email successfully verified! You can now log in." });
+                setMessage("Success");
                 setTimeout(() => {
                     router.push('/login');
-                }, 2000);
+                }, 1000);
             } 
             else 
             {
-                setMessage(data.message || "Verification failed.");
+                toast({ title: "Warning", description: "Email verification failed." });
+                setMessage(data.message || "Email verification failed.");
             }
         }
 
         verify();
     }, [searchParams]);
 
-    return <div className="p-4 text-center">{message}</div>;
-}
+    return (
+    <div className="flex 
+                    flex-col 
+                    items-center 
+                    justify-center 
+                    mt-[230px] 
+                    px-2">
+        <h1 className="font-semibold 
+                        text-2xl 
+                        text-center 
+                        mb-9">
+            <span className="text-blue-500 
+                            font-bold">
+                Account verified. Redirecting you to log in...
+            </span>
+        </h1>
+    </div>
+)}
